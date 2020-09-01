007ebae707d1c1a20ca8cefdb9d488942437b025
atthana-p <atthana.p@codium.co>
2020-09-01 21:28:50 +0700

    Initial project.

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Atthana Personal Post',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Personal Post'),
+    );
+  }
+}
+
+//===================================================
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    print('---------------');
+    print(widget.title);
+    print(widget);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: (){
+            
+          })],
+        ),
+        body: Container());
+  }
+}
